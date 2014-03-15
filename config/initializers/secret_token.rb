8e7c3cc6fdc523c538e3c81e18a0c8ca92c6946a
Paul Scarrone <pscarrone@thinkthroughmath.com>
2014-03-15 00:55:23 -0400

    setting up rails testbed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OpenIdConnectTestbed::Application.config.secret_key_base = '0f9a3ac1b0f2c338abe808df26e37dccdea20dce3ada5d8000c177bed35f8efe5b50eae667ef60835b43c7e91c0c5f501cd8148664a2347ce46a82ac401ec5fe'
