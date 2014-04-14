4629df12daee889d3a0d1f874ea6ae015c48bcc9
Paul Scarrone <pscarrone@thinkthroughmath.com>
2014-04-14 15:28:33 -0400

    dynamic config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,37 +1,24 @@
 OmniAuth.config.logger = Rails.logger
 
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer unless Rails.env.production?
-  #provider :openid_connect, ENV['OA_CLIENT_ID'], ENV['OA_CLIENT_SECRET'],
-  provider :openid_connect, {
-    name: :texas_sso,
-    scope: [:openid, :email, :profile, :address],
-    response_type: :code,
-    client_options: {
-      port: 443,
-      scheme: "https",
-      host: "myprovider.com",
-      identifier: ENV['OA_CLIENT_ID'],
-      secret: ENV['OA_CLIENT_SECRET'],
-      redirect_uri: "/auth/openid_connect/callback",
-    }
+SETUP_PROC = lambda do |env| 
+  request = Rack::Request.new(env)
+  user = User.find_by_subdomain(request.subdomain)
+  env['omniauth.openid_connect'].options[:name] = :texas_sso
+  env['omniauth.openid_connect'].options[:scope] = [:openid, :email, :profile, :address]
+  env['omniauth.openid_connect'].options[:response_type] = :code
+  env['omniauth.openid_connect'].options[:client_options] = {
+    port: 443,
+    scheme: "https",
+    host: "idp.logintex.me",
+    identifier: ENV['OA_CLIENT_ID'],
+    secret: ENV['OA_CLIENT_SECRET'],
+    redirect_uri: "/auth/openid_connect/callback",
   }
 end
 
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :developer unless Rails.env.production?
+  provider :openid_connect, setup: SETUP_PROC
+end
 
-=begin
-config.omniauth :openid_connect, {
-  name: :my_provider,
-  scope: [:openid, :email, :profile, :address],
-  response_type: :code,
-  client_options: {
-    port: 443,
-    scheme: "https",
-    host: "myprovider.com",
-    identifier: ENV["OP_CLIENT_ID"],
-    secret: ENV["OP_SECRET_KEY"],
-    redirect_uri: "http://myapp.com/users/auth/openid_connect/callback",
-  },
-}
-=end
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
